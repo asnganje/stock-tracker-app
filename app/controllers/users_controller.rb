d547a17b251c17ec8877573970a01c4a53030f44
asnganje <abd.nganje@gmail.com>
2025-03-25 17:51:05 +0300

    add self relationship like in social media

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,30 @@ class UsersController < ApplicationController
     # @stocks = current_user.stocks || []
     @stocks = current_user.stocks.to_a
   end
+
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.replace("friend-results", partial: "users/friend_result", locals: { friend: @friend })
+            end
+            format.html do
+              render "users/my_friends"
+              end
+        end
+      else
+        flash[:alert] = "Please enter a valid stock symbol!"
+        redirect_to my_friends_path
+        end   
+    else
+      flash[:alert] = "Please enter a name or email!"
+        redirect_to my_friends_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   def stock_already_tracked?(ticker_symbol)
     stock = Stock.check_db(ticker_symbol)
